5b9b0bfc865131b3f667a21dcf04322cbc25049f
Mayureshpawar29 <pawarmayuresh29@gmail.com>
2024-03-22 16:15:04 +0530

    feat: added simplecov gem for rpsec coverage and adde formatter

--- Gemfile
+++ Gemfile
@@ -22,9 +22,14 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# rspec gem
 gem 'rspec-rails', '~> 6.0.0'
 
+# debugging gem
 gem 'pry'
+
+# test coverage gem
+gem 'simplecov'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,21 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter do |source_file|
+    source_file.lines.count <= 5
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
