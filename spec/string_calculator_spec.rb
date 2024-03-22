ec815a1c64847dedb09a050afcc25010964319fa
Mayureshpawar29 <pawarmayuresh29@gmail.com>
2024-03-22 16:16:49 +0530

    feat: added assertions for the new line input string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculatorController, type: :controller do
-  describe ".add" do
+  describe "#add" do
     context "given an empty string" do
       it "returns sum of the numbers" do
         get :add, params: { numbers: "" }
@@ -34,6 +34,20 @@ RSpec.describe StringCalculatorController, type: :controller do
       it "returns sum of the numbers" do
         get :add, params: { numbers: "1\n2,3" }
         expect(response.parsed_body).to eql(6)
+      end
+    end
+
+    context "given an large number of string 1\n2\n3\n4 " do
+      it "returns sum of the numbers" do
+        get :add, params: { numbers: "1\n2\n3\n4" }
+        expect(response.parsed_body).to eql(10)
+      end
+    end
+
+    context "given an large number of string 1,\n " do
+      it "returns sum of the numbers" do
+        get :add, params: { numbers: "1,\n" }
+        expect(response.parsed_body).to eql(1)
       end
     end
   end
